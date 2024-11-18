9181d750ada45a71f994840d99075a1cd74addf1
duffiwer <duffiwer@gmail.com>
2024-11-18 15:16:33 +0300

    добавлены трейсы

--- app.py
+++ app.py
@@ -5,7 +5,30 @@ from datetime import datetime
 from prometheus_flask_exporter import PrometheusMetrics
 from sqlalchemy.orm import Session
 from prometheus_client import Counter, start_http_server
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 import logging
+
+resource = Resource.create(attributes={
+    "service.name": "bankAPI"
+})
+
+provider = TracerProvider(resource=resource)
+trace.set_tracer_provider(provider)
+
+jaeger_exporter = JaegerExporter(
+    agent_host_name="localhost",
+    agent_port=6831  
+)
+
+span_processor = BatchSpanProcessor(jaeger_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+tracer = trace.get_tracer(__name__)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -39,7 +62,7 @@ http_total_requests = Counter(
     ['method', 'endpoint']  
 )
 
-
+FlaskInstrumentor().instrument_app(app)
 start_http_server(8000)
 
 class User(db.Model):
@@ -63,23 +86,28 @@ class Transaction(db.Model):
 
 @app.route('/users', methods=['POST'])
 def create_user():
-    http_total_requests.labels(method=request.method, endpoint=request.path).inc()
-    logger.info("Creating user started")
-    
-    data = request.get_json()
-    existing_user = User.query.filter_by(email=data['email']).first()
-    
-    if existing_user:
-        logger.warning("Attempt to create user with existing email: %s", data['email'])
-        return jsonify(message="User already exists"), 400
-    
-    new_user = User(username=data['username'], email=data['email'], password=data['password'])
-    db.session.add(new_user)
-    db.session.commit()
-    user_created_total.inc()
-    logger.info("User successfully created: %s", new_user.username)
-    
-    return jsonify(message="User was created"), 201
+    with tracer.start_as_current_span("create_user") as span:
+        http_total_requests.labels(method=request.method, endpoint=request.path).inc()
+        logger.info("Creating user started")
+
+        data = request.get_json()
+        existing_user = User.query.filter_by(email=data['email']).first()
+
+        span.set_attribute("request.email", data['email'])  
+        span.set_attribute("request.username", data['username'])
+
+        if existing_user:
+            span.set_attribute("error", "User already exists")
+            logger.warning("Attempt to create user with existing email: %s", data['email'])
+            return jsonify(message="User already exists"), 400
+
+        new_user = User(username=data['username'], email=data['email'], password=data['password'])
+        db.session.add(new_user)
+        db.session.commit()
+        user_created_total.inc()
+
+        logger.info("User successfully created: %s", new_user.username)
+        return jsonify(message="User was created"), 201
 
 @app.route('/accounts', methods=['POST'])
 def create_account():
@@ -112,44 +140,52 @@ def get_users():
 
 @app.route('/transactions', methods=['POST'])
 def create_transaction():
-    http_total_requests.labels(method=request.method, endpoint=request.path).inc()
-    logger.info("Creating transaction started")
-    
-    data = request.get_json()
-    
-    try:
-        from_account = db.session.get(Account, data['from_account_id'])
-        to_account = db.session.get(Account, data['to_account_id'])
+    with tracer.start_as_current_span("create_transaction") as span:
+        http_total_requests.labels(method=request.method, endpoint=request.path).inc()
+        logger.info("Creating transaction started")
 
-        if not from_account or not to_account:
-            logger.warning("One or both accounts not found: from_account_id=%s, to_account_id=%s", data['from_account_id'], data['to_account_id'])
-            return jsonify(message="One or two accounts not found"), 404
+        data = request.get_json()
 
-        if from_account.balance < data['amount']:
-            logger.warning("Not enough funds for transaction: balance=%s, requested amount=%s", from_account.balance, data['amount'])
-            return jsonify(message="Not enough money"), 400
+        span.set_attribute("transaction.from_account_id", data['from_account_id'])
+        span.set_attribute("transaction.to_account_id", data['to_account_id'])
+        span.set_attribute("transaction.amount", data['amount'])
 
-        transaction = Transaction(
-            from_account_id=data['from_account_id'],
-            to_account_id=data['to_account_id'],
-            amount=data['amount'],
-            currency=data['currency']
-        )
+        try:
+            from_account = db.session.get(Account, data['from_account_id'])
+            to_account = db.session.get(Account, data['to_account_id'])
 
-        from_account.balance -= data['amount']
-        to_account.balance += data['amount']
+            if not from_account or not to_account:
+                span.set_attribute("error", "One or both accounts not found")
+                logger.warning("One or both accounts not found: from_account_id=%s, to_account_id=%s", data['from_account_id'], data['to_account_id'])
+                return jsonify(message="One or two accounts not found"), 404
 
-        db.session.add(transaction)
-        db.session.commit()
-        transactions_total.inc() 
-        logger.info("Transaction successfully created: from account=%s, to account=%s, amount=%s", data['from_account_id'], data['to_account_id'], data['amount'])
+            if from_account.balance < data['amount']:
+                span.set_attribute("error", "Not enough funds")
+                logger.warning("Not enough funds for transaction: balance=%s, requested amount=%s", from_account.balance, data['amount'])
+                return jsonify(message="Not enough money"), 400
 
-        return jsonify(message="Transaction was created"), 201
+            transaction = Transaction(
+                from_account_id=data['from_account_id'],
+                to_account_id=data['to_account_id'],
+                amount=data['amount'],
+                currency=data['currency']
+            )
 
-    except Exception as e:
-        db.session.rollback()
-        logger.error("Error while creating transaction: %s", e)
-        return jsonify(message="Transaction error"), 500
+            from_account.balance -= data['amount']
+            to_account.balance += data['amount']
+
+            db.session.add(transaction)
+            db.session.commit()
+            logger.info("Transaction successfully created: from account=%s, to account=%s, amount=%s", data['from_account_id'], data['to_account_id'], data['amount'])
+
+            return jsonify(message="Transaction was created"), 201
+
+        except Exception as e:
+            db.session.rollback()
+            span.set_attribute("error", str(e))
+            logger.error("Error while creating transaction: %s", e)
+            return jsonify(message="Transaction error"), 500
+
 
 if __name__ == '__main__':
     with app.app_context():
